0b2d81554e6b62918b57846eccba88a388991242
jkjnmlk <123@example.com>
2019-01-17 08:44:25 +0800

    构建基本框架

--- main.c
+++ main.c
@@ -1,9 +1,47 @@
 #include <stdio.h>
+int index=0;
+char arr[1024];
+int main()
+{
 
-int main(){
+    printf("进入敏感字符管理系统\n");
 
-    printf("hello world!");
+    while (1)
+    {
+        printf("1--添加一个敏感字符\n");
+        printf("2--删除最后一个敏感字符\n");
+        printf("3--显示所有敏感字符\n");
+        printf("4--替换所有敏感字符\n");
+        printf("5--退出系统\n");
+        printf("请选择所需项目数字\n");
+        int code;
+        scanf("%d", &code);
+
+        if (code == 1)
+        {
+        //  printf("请输入一个敏感字符\n");
+        //  char m;
+        //  scanf("%c",&m);
+        //  scanf("%c",&m);
+
+        }
+        if (code == 2)
+        {
+            /* code */
+        }
+        if (code == 3)
+        {
+            /* code */
+        }
+        if (code == 4)
+        {
+            /* code */
+        }
+        if (code == 5)
+        {
+            printf("点击回车，退出系统\n");
+        }
+    }
 
     return 0;
-
 }
